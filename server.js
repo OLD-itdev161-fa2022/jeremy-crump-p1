dd7334fc151a8012495b0f9e6b5166156339cf3e
jcrump88 <jcrump88@gmail.com>
2021-10-08 22:02:56 -0500

    Model/API Endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
 
 //Init express
 const app = express();
@@ -7,10 +8,42 @@ const app = express();
 //connect to MongoDB
 connectDatabase();
 
-//API Endpoint
-app.get('/', (req, res)=>
+// Configure Middleware to parse json
+app.use(express.json({ extended: false}));
+
+//API Endpoints
+/** 
+ * @route GET /
+ * @desc Test endpoint
+*/
+
+app.get('/', (req, res) =>
     res.send('Get request sent to API')
 );
+/**
+ * @route POST api/user  
+ * @desc Register user
+ */
+app.post('/api/user',
+[
+    check('name', 'Please enter your name')
+    .not()
+    .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+        'password',
+         'Please enter a password with 3 or more characters'
+        )
+        .isLength({min: 3})
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array()});
+    } else {
+        return res.send(req.body);
+    }
+});
 
 //Listener
 const port = 3000;
